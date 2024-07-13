19ea21b8bb430a058c3b8b91d4328ac489d8414e
Ayush-Petwal <ayushpetwal003@gmail.com>
2024-07-13 12:07:07 +0530

    Refactor app.py for better readability and maintainability

--- app.py
+++ app.py
@@ -6,26 +6,21 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-# Load the wine dataset
+
 wine_df = pd.read_csv('winequality-red.csv')
 
-# Create the predictor (X) and target (y) variables
 X = wine_df.drop('quality', axis=1)
 y = wine_df['quality'].apply(lambda yval: 1 if yval >= 7 else 0)
 
-# Split the dataset
 X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.2, random_state=3)
 
-# Train the Random Forest Classifier model
 model = RandomForestClassifier()
 model.fit(X_train, Y_train)
 
-# Check the accuracy on the test data
 X_test_prediction = model.predict(X_test)
 accuracy = accuracy_score(X_test_prediction, Y_test)
 print(f"Model accuracy: {accuracy:.2f}")
 
-# Function to set background
 def set_background():
     background_css = """
     <style>
@@ -48,7 +43,6 @@ def set_background():
     """
     st.markdown(background_css, unsafe_allow_html=True)
 
-# Function to display header with images
 def display_header():
     left_image_path = "./Pics/2.png"
     right_image_path = "./Pics/3.png"
@@ -66,7 +60,6 @@ def display_header():
         unsafe_allow_html=True
     )
 
-# Main app
 def main():
     set_background()
     display_header()
@@ -99,49 +92,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
-
-
-
-
-
-# import numpy as np
-# import pandas as pd
-# import matplotlib.pyplot as plt
-# import seaborn as sns
-# from sklearn.model_selection import train_test_split
-# from sklearn.ensemble import RandomForestClassifier
-# from sklearn.metrics import accuracy_score
-# import streamlit as st
-
-# # Load the wine dataset
-# wine_df = pd.read_csv('winequality-red.csv')
-
-# # Create the predictor (X) and target (y) variables
-# X = wine_df.drop('quality', axis=1)
-# y = wine_df['quality'].apply(lambda yval: 1 if yval >= 7 else 0)
-
-
-# X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.2, random_state=3)
-# # Train the Random Forest Classifier model
-# model = RandomForestClassifier()
-# model.fit(X_train, Y_train)
-# # accuracy on test data
-# X_test_prediction = model.predict(X_test)
-# print(accuracy_score(X_test_prediction, Y_test))
-
-# # web app
-# st.title("Wine Quality Prediction Model")
-# input_text = st.text_input('Enter all Wine Features')
-# input_text_list = input_text.split(',')
-# features = np.asarray(input_text_list)
-# prediction = model.predict(features.reshape(1,-1))
-# if prediction[0] == 1:
-#     st.write("Good Quality Wine")
-# else:
-#     st.write("Bad Quality Wine")
