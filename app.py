ebf271857cbab1a8fd9bd1ebc4597716f1ae5fc4
Ayush-Petwal <ayushpetwal003@gmail.com>
2024-07-13 10:13:33 +0530

    Wine Quality Prediction

---
+++ app.py
@@ -0,0 +1,147 @@
+import numpy as np
+import pandas as pd
+import base64
+import streamlit as st
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+# Load the wine dataset
+wine_df = pd.read_csv('winequality-red.csv')
+
+# Create the predictor (X) and target (y) variables
+X = wine_df.drop('quality', axis=1)
+y = wine_df['quality'].apply(lambda yval: 1 if yval >= 7 else 0)
+
+# Split the dataset
+X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.2, random_state=3)
+
+# Train the Random Forest Classifier model
+model = RandomForestClassifier()
+model.fit(X_train, Y_train)
+
+# Check the accuracy on the test data
+X_test_prediction = model.predict(X_test)
+accuracy = accuracy_score(X_test_prediction, Y_test)
+print(f"Model accuracy: {accuracy:.2f}")
+
+# Function to set background
+def set_background():
+    background_css = """
+    <style>
+    .stApp {
+        background: linear-gradient(to right, #722f37 , #54121a , #4f0303);
+    }
+    .header {
+        display: flex;
+        align-items: center;
+        border-bottom: 2px solid #3a0101 ;
+    }
+    .header h1{
+        margin-top : 10px;
+        font-family: cursive;
+    }
+    .header img {
+        height: 80px;
+    }
+    </style>
+    """
+    st.markdown(background_css, unsafe_allow_html=True)
+
+# Function to display header with images
+def display_header():
+    left_image_path = "./Pics/2.png"
+    right_image_path = "./Pics/3.png"
+    left_image_base64 = base64.b64encode(open(left_image_path, "rb").read()).decode()
+    right_image_base64 = base64.b64encode(open(right_image_path, "rb").read()).decode()
+    
+    st.markdown(
+        f"""
+        <div class="header">
+            <img src="data:image/png;base64,{left_image_base64}" class="img-fluid">
+            <h1>Wine Quality Prediction</h1>
+            <img src="data:image/png;base64,{right_image_base64}" class="img-fluid">
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
+
+# Main app
+def main():
+    set_background()
+    display_header()
+
+    st.write("""
+    ## Predict the Quality of Wine
+    """)
+    
+    features = {}
+    features['fixed acidity'] = st.number_input('Fixed Acidity', 4.0, 15.0, 7.4)
+    features['volatile acidity'] = st.number_input('Volatile Acidity', 0.1, 1.5, 0.7)
+    features['citric acid'] = st.number_input('Citric Acid', 0.0, 1.0, 0.0)
+    features['residual sugar'] = st.number_input('Residual Sugar', 1.0, 15.0, 2.5)
+    features['chlorides'] = st.number_input('Chlorides', 0.01, 0.1, 0.07)
+    features['free sulfur dioxide'] = st.number_input('Free Sulfur Dioxide', 1, 50, 30)
+    features['total sulfur dioxide'] = st.number_input('Total Sulfur Dioxide', 5, 200, 100)
+    features['density'] = st.number_input('Density', 0.990, 1.005, 0.996)
+    features['pH'] = st.number_input('pH', 2.5, 4.0, 3.3)
+    features['sulphates'] = st.number_input('Sulphates', 0.3, 2.0, 0.7)
+    features['alcohol'] = st.number_input('Alcohol', 8.0, 15.0, 10.0)
+
+
+    if st.button('Predict'):
+        feature_values = np.array([features[feature] for feature in X.columns])
+        prediction = model.predict(feature_values.reshape(1, -1))
+        if prediction[0] == 1:
+            st.write(""" ## Good Quality Wine 🍷🥂 """)
+        else:
+            st.write(""" ## Bad Quality Wine ❌ """)
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
+
+
+
+# import numpy as np
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+# from sklearn.model_selection import train_test_split
+# from sklearn.ensemble import RandomForestClassifier
+# from sklearn.metrics import accuracy_score
+# import streamlit as st
+
+# # Load the wine dataset
+# wine_df = pd.read_csv('winequality-red.csv')
+
+# # Create the predictor (X) and target (y) variables
+# X = wine_df.drop('quality', axis=1)
+# y = wine_df['quality'].apply(lambda yval: 1 if yval >= 7 else 0)
+
+
+# X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.2, random_state=3)
+# # Train the Random Forest Classifier model
+# model = RandomForestClassifier()
+# model.fit(X_train, Y_train)
+# # accuracy on test data
+# X_test_prediction = model.predict(X_test)
+# print(accuracy_score(X_test_prediction, Y_test))
+
+# # web app
+# st.title("Wine Quality Prediction Model")
+# input_text = st.text_input('Enter all Wine Features')
+# input_text_list = input_text.split(',')
+# features = np.asarray(input_text_list)
+# prediction = model.predict(features.reshape(1,-1))
+# if prediction[0] == 1:
+#     st.write("Good Quality Wine")
+# else:
+#     st.write("Bad Quality Wine")
